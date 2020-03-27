23174ae02f6b4648693141db9051749b63426c38
tpellegrin <tgpellegrin@gmail.com>
2020-03-27 19:15:02 -0300

    Add pages as components

---
+++ frontend/src/pages/Profile/index.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { FiPower, FiTrash2 } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+
+import logoImg from '../../assets/logo.svg';
+import './styles.css'
+
+export default function Profile() {
+    return (
+        <div className="profile-container">
+            <header>
+                <img src={logoImg} alt="Be The Hero" />
+                <span>Bem-vinda, ONG</span>
+                <Link className="button" to="/incidents/new">Cadastro</Link>
+                <button type="button">
+                    <FiPower size={18} color="#E02041" />
+                </button>
+            </header>
+            <h1>Casos cadastrados</h1>
+            <ul>
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Caso teste</p>
+                    <strong>DESCRIÇÃO</strong>
+                    <p>Descrição teste</p>
+                    <strong>VALOR</strong>
+                    <p>R$ 120,00</p>
+                    <button type="button">
+                        <FiTrash2 size={20} color="#a8a8b3" />
+                    </button>
+                </li>
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Caso teste</p>
+                    <strong>DESCRIÇÃO</strong>
+                    <p>Descrição teste</p>
+                    <strong>VALOR</strong>
+                    <p>R$ 120,00</p>
+                    <button type="button">
+                        <FiTrash2 size={20} color="#a8a8b3" />
+                    </button>
+                </li>
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Caso teste</p>
+                    <strong>DESCRIÇÃO</strong>
+                    <p>Descrição teste</p>
+                    <strong>VALOR</strong>
+                    <p>R$ 120,00</p>
+                    <button type="button">
+                        <FiTrash2 size={20} color="#a8a8b3" />
+                    </button>
+                </li>
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Caso teste</p>
+                    <strong>DESCRIÇÃO</strong>
+                    <p>Descrição teste</p>
+                    <strong>VALOR</strong>
+                    <p>R$ 120,00</p>
+                    <button type="button">
+                        <FiTrash2 size={20} color="#a8a8b3" />
+                    </button>
+                </li>
+            </ul>
+        </div>
+    );
+}
