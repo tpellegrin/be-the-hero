72577153c4b18dfeda8c81fc66f1b2f0f3ef2ae6
tpellegrin <tgpellegrin@gmail.com>
2020-03-28 16:41:55 -0300

    Fix missing returned ID from new records stored

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -26,15 +26,15 @@ module.exports = {
 
     async create(req, res) {
         const { title, description, value } = req.body;
-        const ong_id = request.headers.authorization;
+        const ong_id = req.headers.authorization;
 
         const [id] = await connection('incidents').insert({
             title,
             description,
             value,
-            ong_id
-        });
-
+            ong_id,
+        }).returning('id');
+ 
         return res.json({ id });
     },
 
@@ -48,11 +48,11 @@ module.exports = {
             .first()
 
         if (incident.ong_id !== ong_id) {
-            return response.status(401).json({ error: 'Operation not permitted.' });
+            return res.status(401).json({ error: 'Operation not permitted.' });
         } 
 
         await connection('incidents').where('id', id).delete();
 
-        return response.status(204).send();
+        return res.status(204).send();
     }
 };
