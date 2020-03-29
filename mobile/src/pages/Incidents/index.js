94e8efa2810bd183e779ed2c39092f35d5a815bf
tpellegrin <tgpellegrin@gmail.com>
2020-03-29 17:00:23 -0300

    Add mockup Incidents with stylization

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, Image, Text } from 'react-native';
+import { View, FlatList, Image, Text, TouchableOpacity } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
 import logoImg from '../../assets/logo.png';
 import styles from './styles';
@@ -15,6 +16,29 @@ export default function Incidents() {
             </View>
             <Text style={styles.title}>Bem-vindo!</Text>
             <Text style={styles.description}>Escolha um dos casos abaixo e salve o dia.</Text>
+            <FlatList
+                style={styles.incidentList}
+                keyExtractor={incident => String(incident)}
+                showsVerticalScrollIndicator={false}
+                data={[1, 2, 3]}
+                renderItem={() => (
+                    <View style={styles.incident}>
+                        <Text style={styles.incidentProperty}>ONG:</Text>
+                        <Text style={styles.incidentValue}>ong</Text>
+                        <Text style={styles.incidentProperty}>CASO:</Text>
+                        <Text style={styles.incidentValue}>Cadela atropelada</Text>
+                        <Text style={styles.incidentProperty}>VALOR:</Text>
+                        <Text style={styles.incidentValue}>R$ 120,00</Text>
+                        <TouchableOpacity 
+                            style={styles.detailsButton} 
+                            onPress={() => {}}
+                        >
+                            <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+                            <Feather name="arrow-right" size={16} color="#E02041" />
+                        </TouchableOpacity>
+                    </View>
+                )}
+            />
         </View>
     );
 }
