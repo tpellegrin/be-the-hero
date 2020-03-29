b2cd55469f5d49cbeefcf24b001bf69dd7bdc663
tpellegrin <tgpellegrin@gmail.com>
2020-03-28 23:37:29 -0300

    Fix typo

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -60,7 +60,7 @@ export default function Profile() {
                         <strong>DESCRIÇÃO</strong>
                         <p>{incident.description}</p>
                         <strong>VALOR</strong>
-                        <p>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incidents.value)}</p>
+                        <p>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.value)}</p>
                         <button onClick={() => handleDeleteIncident(incident.id)} type="button">
                             <FiTrash2 size={20} color="#a8a8b3" />
                         </button>
