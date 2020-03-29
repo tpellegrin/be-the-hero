9da07dcb7cff609d9f88336f946e0f0b5bc9d0d5
tpellegrin <tgpellegrin@gmail.com>
2020-03-29 13:34:09 -0300

    Update Incidents page style

--- mobile/src/pages/Incidents/styles.js
+++ mobile/src/pages/Incidents/styles.js
@@ -5,5 +5,35 @@ export default StyleSheet.create({
     container: {
         flex: 1,
         paddingHorizontal: 24,
+        paddingTop: Constants.statusBarHeight + 20,
+    },
+
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    },
+
+    headerText: {
+        fontSize: 15,
+        color: '#737380',
+    },
+
+    headerTextBold: {
+        fontWeight: 'bold',
+    },
+
+    title: {
+        fontSize: 30,
+        marginBottom: 16,
+        marginTop: 48,
+        color: '#13131a',
+        fontWeight: 'bold',
+    },
+
+    description: {
+        fontSize: 16,
+        lineHeight: 24,
+        color: '#737380',
     }
 });
