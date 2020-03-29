46e5737ee359b8ca32202c71663f2cf9a8f2f7db
tpellegrin <tgpellegrin@gmail.com>
2020-03-28 23:12:09 -0300

    Add logout functionality

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 import api from '../../services/api';
 import logoImg from '../../assets/logo.svg';
 import './styles.css'
 
 export default function Profile() {
+    const history = useHistory();
     const [incidents, setIncidents] = useState([]);
     const ongId = localStorage.getItem('ongId');
     const ongName = localStorage.getItem('ongName');
@@ -35,13 +36,18 @@ export default function Profile() {
         } 
     }
 
+    function handleLogout() {
+        localStorage.clear();
+        history.push('/');
+    }
+
     return (
         <div className="profile-container">
             <header>
                 <img src={logoImg} alt="Be The Hero" />
                 <span>Bem-vinda, {ongName}</span>
                 <Link className="button" to="/incidents/new">Cadastro</Link>
-                <button type="button">
+                <button onClick={handleLogout} type="button">
                     <FiPower size={18} color="#E02041" />
                 </button>
             </header>
