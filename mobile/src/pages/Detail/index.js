45b58084f12702214f9de9393d3c115ea7d4fb9e
tpellegrin <tgpellegrin@gmail.com>
2020-03-29 18:16:38 -0300

    Add back navigation from Detail

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 import { View, Image, Text, TouchableOpacity } from 'react-native';
 
 import logoImg from '../../assets/logo.png';
 import styles from './styles';
 
 export default function Detail() {
+    const navigation = useNavigation();
+
+    function navigateBack() {
+        navigation.goBack();
+    }
+
     return (
         <View style={styles.container}>
             <View style={styles.header}>
                 <Image source={logoImg} />
-                <TouchableOpacity onPress={() => {}}>
+                <TouchableOpacity onPress={navigateBack}>
                     <Feather name="arrow-left" size={28} color="#E02041" /> 
                 </TouchableOpacity>
             </View>
